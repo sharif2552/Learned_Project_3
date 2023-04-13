be09817f6e217c828d0281c0b8a64a36f89d8a28
Sharif <sharifhossain4888@gmail.com>
2023-04-13 16:00:32 +0600

    halka changes

---
+++ ersPCSharifvenvLearned_project_3newappadmin.py
@@ -0,0 +1,23 @@
+[33mcommit 1268b03ab758f18ed8ab81071dce3dac640d2127[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31mDjangoProject/master[m[33m)[m
+Author: Sharif <sharifhossain4888@gmail.com>
+Date:   Thu Mar 30 14:18:35 2023 +0600
+
+    requirements
+
+[33mcommit 840458fe20742253f36275e210ac19bdbe14c33f[m[33m ([m[1;31mDjangoProject/branch1[m[33m)[m
+Author: Sharif <sharifhossain4888@gmail.com>
+Date:   Wed Mar 29 13:41:41 2023 +0600
+
+    3rd
+
+[33mcommit d9483ab8ccb566bd57c4a995981da7b12ce3d63c[m
+Author: Sharif <sharifhossain4888@gmail.com>
+Date:   Wed Mar 29 13:40:04 2023 +0600
+
+    2nd
+
+[33mcommit 5966571c3c3a620056e139cb76a1e52aa6f8f6cb[m
+Author: Sharif <sharifhossain4888@gmail.com>
+Date:   Wed Mar 29 13:36:29 2023 +0600
+
+    first commit
